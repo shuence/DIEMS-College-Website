c953fba6b73dc9069975779d66a290a78e791900
shuence <Shubhampitekar98@gmail.com>
2023-03-09 13:29:37 +0530

    Removed unnecessary code

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import leftArrow from "../../assests/images/leftArrow.png";
+import rightArrow from "../../assests/images/rightArrow.png";
+import { sliderData } from "../../Data/sliderData";
+
+const Home = () => {
+  return (
+    <>
+        <h1>Hello</h1>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/Data/sliderData.jsx
@@ -0,0 +1,22 @@
+export const testimonialsData = [
+  {
+    url: "https://www.dietms.org/wp-content/uploads/2019/12/image56-1024x682.png",
+    heading: "Placement 2018-2019",
+    des: "Sample Description",
+  },
+  {
+    url: "https://www.dietms.org/wp-content/uploads/2020/01/IMG_5914-Copy.jpg",
+    heading: "Cycling Club",
+    des: "Sample Description",
+  },
+  {
+    url: "https://www.dietms.org/wp-content/uploads/2019/12/Photo-Mech-Gokart-1024x683.jpg",
+    heading: "Mechanical Gokart",
+    des: "Sample Description",
+  },
+  {
+    url: "https://www.dietms.org/wp-content/uploads/2019/12/IMG_6813.jpg",
+    heading: "Felication of guest",
+    des: "Sample Description",
+  },
+];
