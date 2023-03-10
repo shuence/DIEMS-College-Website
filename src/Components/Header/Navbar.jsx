d48c10fe3e0170ba96e26c63e25a2bc814308ade
shuence <Shubhampitekar98@gmail.com>
2023-03-10 18:27:17 +0530

    Small change

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -4,306 +4,126 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <nav
-        id="mainNav"
-        className="mb-6 z-10 h-auto w-full bg-white hidden lg:block z-index:99 swiper-pagination"
-        data-v-572fd66e=""
-      >
+      <nav id="mainNav" className="mb-6 z-10 h-auto w-full bg-white hidden lg:block z-index:99 swiper-pagination" data-v-572fd66e="">
         <div className="h-auto w-full z-40 px-6" data-v-572fd66e="">
-          <div
-            className=" bg-white flex flex-row justify-between items-center py-1 md:py-3 text-sm text-white relative space-x-4"
-            data-v-572fd66e=""
-          >
-            <div
-              className="w-1/6 bg-primaryDark h-10 shadow flex items-center"
-              data-v-572fd66e=""
-            >
-              <div
-                className="w-full flex flex-row items-center justify-center"
-                data-v-572fd66e=""
-              >
-                <a
-                  href="/"
-                  aria-current="page"
-                  className="active--exact active"
-                  data-v-572fd66e=""
-                >
-                  <p className="text-2xl font-semibold" data-v-572fd66e="">
-                    DIEMS
-                  </p>
+          <div className=" bg-white flex flex-row justify-between items-center py-1 md:py-3 text-sm text-white relative space-x-4" data-v-572fd66e="">
+            <div className="w-1/6 bg-primaryDark h-10 shadow flex items-center" data-v-572fd66e="">
+              <div className="w-full flex flex-row items-center justify-center" data-v-572fd66e="">
+                <a href="/" aria-current="page" className="active--exact active" data-v-572fd66e="">
+                  <p className="text-2xl font-semibold" data-v-572fd66e="">DIEMS</p>
                 </a>
               </div>
             </div>
             <div className="w-5/6 bg-gray-300 shadow " data-v-572fd66e="">
               <div className="hidden md:block h-10 w-full" data-v-572fd66e="">
-                <div
-                  className="flex items-center text-gray-400 font-light w-full "
-                  data-v-572fd66e=""
-                >
-                  <div
-                    className="flex flex-row justify-center w-full text-primary "
-                    data-v-572fd66e=""
-                  >
+                <div className="flex items-center text-gray-400 font-light w-full " data-v-572fd66e="">
+                  <div className="flex flex-row justify-center w-full text-primary " data-v-572fd66e="">
                     <span data-v-572fd66e="">
                       <div className="mt-2" data-v-572fd66e="">
-                        <a
-                          href="/"
-                          aria-current="page"
-                          className="hover:text-primaryDark font-medium active--exact active"
-                          data-v-572fd66e=""
-                        >
+                        <a href="/" aria-current="page" className="hover:text-primaryDark font-medium active--exact active" data-v-572fd66e="">
                           Home
                         </a>
                       </div>
                     </span>
                   </div>
-                  <div
-                    className="flex flex-row justify-center w-full text-primary "
-                    data-v-572fd66e=""
-                  >
+                  <div className="flex flex-row justify-center w-full text-primary " data-v-572fd66e="">
                     <span data-v-572fd66e="">
                       <div className="mt-2" data-v-572fd66e="">
-                        <button
-                          className="hover:text-primaryDark font-medium flex flex-row items-center"
-                          data-v-572fd66e=""
-                        >
-                          Programs
-                          <i
-                            className="fas fa-caret-down ml-1"
-                            data-v-572fd66e=""
-                            aria-hidden="true"
-                          ></i>
+                        <button className="hover:text-primaryDark font-medium flex flex-row items-center" data-v-572fd66e="">
+                          About us
+                          <i className="fas fa-caret-down ml-1" data-v-572fd66e="" aria-hidden="true"></i>
                         </button>
                       </div>
                     </span>
                   </div>
-                  <div
-                    className=" flex flex-row justify-center w-full text-primary"
-                    data-v-572fd66e=""
-                  >
+                  <div className="flex flex-row justify-center w-full text-primary" data-v-572fd66e="">
                     <span data-v-572fd66e="">
                       <div className="mt-2" data-v-572fd66e="">
-                        <a
-                          href="/iirc"
-                          className="hover:text-primaryDark font-medium"
-                          data-v-572fd66e=""
-                        >
-                          IIRC
-                        </a>
-                      </div>
-                    </span>
-                  </div>
-                  <div
-                    className="flex flex-row justify-center w-full text-primary"
-                    data-v-572fd66e=""
-                  >
-                    <span data-v-572fd66e="">
-                      <div className="mt-2" data-v-572fd66e="">
-                        <button
-                          className="hover:text-primaryDark font-medium flex flex-row items-center"
-                          data-v-572fd66e=""
-                        >
-                          About Us
-                          <i
-                            className="fas fa-caret-down ml-1"
-                            data-v-572fd66e=""
-                            aria-hidden="true"
-                          ></i>
-                        </button>
-                      </div>
-                    </span>
-                  </div>
-                  <div
-                    className="flex flex-row justify-center w-full text-primary"
-                    data-v-572fd66e=""
-                  >
-                    <span data-v-572fd66e="">
-                      <div className="mt-2" data-v-572fd66e="">
-                        <button
-                          className="hover:text-primaryDark font-medium flex flex-row items-center"
-                          data-v-572fd66e=""
-                        >
+                        <button className="hover:text-primaryDark font-medium flex flex-row items-center" data-v-572fd66e="">
                           Admissions
-                          <i
-                            className="fas fa-caret-down ml-1"
-                            data-v-572fd66e=""
-                            aria-hidden="true"
-                          ></i>
+                          <i className="fas fa-caret-down ml-1" data-v-572fd66e="" aria-hidden="true"></i>
                         </button>
                       </div>
                     </span>
                   </div>
-                  <div
-                    className="
-                    flex flex-row
-                    justify-center
-                    w-full
-                    text-primary
-                  "
-                    data-v-572fd66e=""
-                  >
+                  <div className=" flex flex-row justify-center w-full text-primary" data-v-572fd66e="">
                     <span data-v-572fd66e="">
                       <div className="mt-2" data-v-572fd66e="">
-                        <button
-                          className="
-                          hover:text-primaryDark
-                          font-medium
-                          flex flex-row
-                          items-center
-                        "
-                          data-v-572fd66e=""
-                        >
-                          Campus{" "}
-                          <i
-                            className="fas fa-caret-down ml-1"
-                            data-v-572fd66e=""
-                            aria-hidden="true"
-                          ></i>
+                        <button className=" hover:text-primaryDark font-medium flex flex-row items-center" data-v-572fd66e="">
+                          Programs
+                          <i className="fas fa-caret-down ml-1" data-v-572fd66e="" aria-hidden="true"></i>
                         </button>
                       </div>
                     </span>
                   </div>
-                  <div
-                    className="
-                    flex flex-row
-                    justify-center
-                    w-full
-                    text-primary
-                  "
-                    data-v-572fd66e=""
-                  >
+                  <div className=" flex flex-row justify-center w-full text-primary" data-v-572fd66e="">
                     <span data-v-572fd66e="">
                       <div className="mt-2" data-v-572fd66e="">
-                        <button
-                          className="
-                          hover:text-primaryDark
-                          font-medium
-                          flex flex-row
-                          items-center
-                        "
-                          data-v-572fd66e=""
-                        >
-                          Research{" "}
-                          <i
-                            className="fas fa-caret-down ml-1"
-                            data-v-572fd66e=""
-                            aria-hidden="true"
-                          ></i>
+                        <button className=" hover:text-primaryDark font-medium flex flex-row items-center" data-v-572fd66e="">
+                          Campus
+                          <i className="fas fa-caret-down ml-1" data-v-572fd66e="" aria-hidden="true"></i>
                         </button>
                       </div>
                     </span>
                   </div>
-                  <div
-                    className="
-                    flex flex-row
-                    justify-center
-                    w-full
-                    text-primary
-                  "
-                    data-v-572fd66e=""
-                  >
+                  <div className=" flex flex-row justify-center w-full text-primary" data-v-572fd66e="">
                     <span data-v-572fd66e="">
                       <div className="mt-2" data-v-572fd66e="">
-                        <a
-                          href="/library"
-                          className="hover:text-primaryDark font-medium"
-                          data-v-572fd66e=""
-                        >
-                          Library
+                        <button className=" hover:text-primaryDark font-medium flex flex-row items-center" data-v-572fd66e="">
+                          IQAC
+                          <i className="fas fa-caret-down ml-1" data-v-572fd66e="" aria-hidden="true"></i>
+                        </button>
+                      </div>
+                    </span>
+                  </div>
+                  <div className=" flex flex-row justify-center w-full text-primary " data-v-572fd66e="">
+                    <span data-v-572fd66e="">
+                      <div className="mt-2" data-v-572fd66e="">
+                        <a href="/library" className="hover:text-primaryDark font-medium" data-v-572fd66e="">
+                          NIRF
                         </a>
                       </div>
                     </span>
                   </div>
-                  <div
-                    className="
-                    flex flex-row
-                    justify-center
-                    w-full
-                    text-primary
-                  "
-                    data-v-572fd66e=""
-                  >
+                  <div className=" flex flex-row justify-center w-full text-primary " data-v-572fd66e="" >
                     <span data-v-572fd66e="">
                       <div className="mt-2" data-v-572fd66e="">
-                        <a
-                          href="/placement"
-                          className="hover:text-primaryDark font-medium"
-                          data-v-572fd66e=""
-                        >
+                        <a href="/placement" className="hover:text-primaryDark font-medium" data-v-572fd66e="" >
                           Placement
                         </a>
                       </div>
                     </span>
                   </div>
-                  <div
-                    className="
-                    flex flex-row
-                    justify-center
-                    w-full
-                    text-primary
-                  "
-                    data-v-572fd66e=""
-                  >
+                  <div className=" flex flex-row justify-center w-full text-primary" data-v-572fd66e="">
                     <span data-v-572fd66e="">
                       <div className="mt-2" data-v-572fd66e="">
-                        <a
-                          href="/alumni"
-                          className="hover:text-primaryDark font-medium"
-                          data-v-572fd66e=""
-                        >
-                          Alumni
-                        </a>
+                        <button className=" hover:text-primaryDark font-medium flex flex-row items-center" data-v-572fd66e="">
+                          Others
+                          <i className="fas fa-caret-down ml-1" data-v-572fd66e="" aria-hidden="true"></i>
+                        </button>
                       </div>
                     </span>
                   </div>
-                </div>
+                  </div>
               </div>
             </div>
           </div>
         </div>
         <div className="px-2 lg:px-6 w-full mt-0 block" data-v-572fd66e="">
-          <div
-            className="
-            bg-gray-300
-            text-primary
-            h-10
-            overflow-hidden
-            relative
-            flex
-            items-center
-            shadow
-            border-b-1 border-secondary
-          "
-            data-v-572fd66e=""
-          >
+          <div className=" bg-gray-300 text-primary h-10 overflow-hidden relative flex items-center shadow border-b-1 border-secondary " data-v-572fd66e="" >
             <div className="w-full overflow-hidden">
               <div className="inline-block morquee animate-marquee">
                 <span data-v-572fd66e="">
-                  <span
-                    data-v-572fd66e=""
-                    className="px-2 border-r-2 border-secondary text-tiny"
-                  >
+                  <span data-v-572fd66e="" className="px-2 border-r-2 border-secondary text-tiny" >
                     Some Notification
-                    <a
-                      data-v-572fd66e=""
-                      href="http://www.dietms.com"
-                      target="__blank"
-                      className=" bg-secondary text-primaryDark text-xxs"
-                    ></a>
+                    <a data-v-572fd66e="" href="http://www.dietms.com" target="__blank" className=" bg-secondary text-primaryDark text-xxs" >
+                    </a>
                   </span>
                 </span>
                 <span data-v-572fd66e="">
-                  <span
-                    data-v-572fd66e=""
-                    className="px-2 border-r-2 border-secondary text-tiny"
-                  >
+                  <span data-v-572fd66e="" className="px-2 border-r-2 border-secondary text-tiny" >
                     Sample Notification
-                    <a
-                      data-v-572fd66e=""
-                      href="https://dbatu.ac.in"
-                      target="__blank"
-                      className=" bg-secondary text-primaryDark text-xxs"
-                    ></a>
+                    <a data-v-572fd66e="" href="https://dbatu.ac.in" target="__blank" className=" bg-secondary text-primaryDark text-xxs" ></a>
                   </span>
                 </span>
               </div>
